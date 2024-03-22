11c56f8cdbf1178afffa3be1e89b3145cab71306
Bohan <78827836+s026352@users.noreply.github.com>
2024-03-22 14:59:56 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Banner />
         
         <Projects />
-        <Skills />
+        {/* <Skills /> */}
       </div>
       
       {/* <Contact />
--- src/component/Project.js
+++ src/component/Project.js
@@ -6,6 +6,10 @@ import projImg3 from "../assets/img/trucking.png";
 import projImg4 from "../assets/img/211Robot.png";
 import projImg6 from "../assets/img/Claw.jpg";
 import projImg5 from "../assets/img/assetplus.png";
+import mazeimg from "../assets/img/game2.png";
+import cyberimg from "../assets/img/cyber.png";
+import punchimg from "../assets/img/punch.png";
+import cityimg from "../assets/img/citysw.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -29,6 +33,12 @@ export const Projects = () => {
       description: "Robot used to setup the rocket onto the platform, Controlled by self designed web-interface",
       imgUrl: projImg6,
     },
+    {
+      title: "City Sweeper",
+      description: "An automated caterpillar robot equipped with a sweeping mechanism, capable of classifying waste autonomously.",
+      imgUrl: cityimg,
+    },
+
 
   ];
   const projects2 = [
@@ -39,14 +49,24 @@ export const Projects = () => {
     },
     {
       title: "Trucking",
-      description: "A Software matches the trucker with broker and max the profit ",
+      description: "A Software matches the trucker with broker and max the profit",
       imgUrl: projImg3,
     },
-    // {
-    //   title: "16 bit CPU",
-    //   description: "can process 16bits instructions from PC",
-    //   imgUrl: projImg2,
-    // },
+    {
+      title: "Punch My Prof",
+      description: "Hand Interaction Game with Unity, utilize Meshy API through generate 3D prof object through 2D pictures",
+      imgUrl: punchimg,
+    },
+    {
+      title: "CyberSight",
+      description: "Vision AI to guide users in real-time to find and confirm the acquisition of objects using visual and auditory feedback through mobile devices.",
+      imgUrl: cyberimg,
+    },
+    {
+      title: "Maze Game",
+      description: "Imployed assembly and c to control little \"&\" to escape.",
+      imgUrl: mazeimg,
+    },
 
   ];
   const projects3 = [
@@ -112,6 +132,7 @@ export const Projects = () => {
                           projects2.map((project, index) => {
                             return (
                               <ProjectCard
+                      
                                 key={index}
                                 {...project}
                                 />
